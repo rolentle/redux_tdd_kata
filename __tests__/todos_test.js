b06bfaab2f3fb0d98c31085631fad799bd477112
Rolen Le <rolentle@gmail.com>
2017-03-14 23:25:11 -0400

    Add addTodo Functionality

--- __tests__/todos_test.js
+++ __tests__/todos_test.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux';
-import Todos from '../src/todos';
+import Todos, { ADD_TODO } from '../src/todos';
 
 describe("Todos", () => {
   it("starts off with no todos", () => {
@@ -7,5 +7,14 @@ describe("Todos", () => {
     const state = store.getState();
     const todos = state.todos;
     expect(todos).toEqual([]);
+  });
+
+  it("adds a todo", () => {
+    const store = createStore(Todos);
+    const todo = { text: 'foo' }
+    store.dispatch({type: ADD_TODO, payload: { todo }});
+    const todos = store.getState().todos;
+    expect(todos.length).toEqual(1);
+    expect(todos[0].text).toEqual('foo');
   });
 });
--- src/todos.js
+++ src/todos.js
@@ -1,4 +1,14 @@
 const initialState = {todos: []};
+export const ADD_TODO = 'ADD_TODO';
+
+function addTodo(state, todo) {
+      return Object.assign({}, state, { todos: [todo, ...state.todos]});
+}
 export default function(state=initialState, action) {
-  return state;
+  switch(action.type) {
+    case  ADD_TODO:
+      return addTodo(state, action.payload.todo);
+    default:
+      return state;
+  }
 };
