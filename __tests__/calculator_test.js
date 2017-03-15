1142f8026568aadeaeb1db52128a5128e6013054
Rolen Le <rolentle@gmail.com>
2017-03-14 23:02:42 -0400

    Namespace everything to Calculator

--- __tests__/calculator_test.js
+++ __tests__/calculator_test.js
@@ -1,22 +1,22 @@
 import { createStore } from 'redux';
-import Caculator, { ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION } from '../src/index';
+import Calculator, { ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION } from '../src/index';
 
-describe("Caculator", () => {
+describe("Calculator", () => {
   it("initalizes at 0", () => {
-    const store = createStore(Caculator);
+    const store = createStore(Calculator);
     const state = store.getState();
     expect(state).toBe(0);
   });
 
   it("can add 0 + 1 = 1", () => {
-    const store = createStore(Caculator);
+    const store = createStore(Calculator);
     store.dispatch({type: ADDITION, payload: 1 });
     const state = store.getState();
     expect(state).toBe(1);
   });
 
   it("can add 1 + 1 = 2", () => {
-    const store = createStore(Caculator);
+    const store = createStore(Calculator);
     store.dispatch({type: ADDITION, payload: 1 });
     store.dispatch({type: ADDITION, payload: 1 });
     const state = store.getState();
@@ -24,21 +24,21 @@ describe("Caculator", () => {
   });
 
   it("can substract 0 - 1 = -1", () => {
-    const store = createStore(Caculator);
+    const store = createStore(Calculator);
     store.dispatch({type: SUBTRACTION, payload: 1 });
     const state = store.getState();
     expect(state).toBe(-1);
   });
 
   it("can multiple  2 * 2 = 4", () => {
-    const store = createStore(Caculator, 2);
+    const store = createStore(Calculator, 2);
     store.dispatch({type: MULTIPLICATION, payload: 2 });
     const state = store.getState();
     expect(state).toBe(4);
   });
 
   it("can divide  2 / 2 = 1", () => {
-    const store = createStore(Caculator, 2);
+    const store = createStore(Calculator, 2);
     store.dispatch({type: DIVISION, payload: 2 });
     const state = store.getState();
     expect(state).toBe(1);
